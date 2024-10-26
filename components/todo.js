30ad75a6560ff3108a6d8680350910763494bef1
Kira Matsuo <kira.nm.matsuo@gmail.com>
2024-10-25 18:05:48 -0700

    feat checkbox & delete event listeners

--- components/todo.js
+++ components/todo.js
@@ -4,12 +4,25 @@ class Todo {
     this._templateElement = document.querySelector(selector);
   }
 
-  generateCheckboxEl() {
-    const todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
-    const todoLabel = this._todoElement.querySelector(".todo__label");
-    todoCheckboxEl.checked = this._data.completed;
-    todoCheckboxEl.id = `todo-${this._data.id}`;
-    todoLabel.setAttribute("for", `todo-${this._data.id}`);
+  _setEventListeners() {
+    // check boxes //
+    this._todoCheckboxEl.addEventListener("click", () => {
+      this._data.completed = !this._data.completed;
+      console.log(this._data.completed);
+    });
+
+    // delete button //
+    this._todoDeleteBtn.addEventListener("click", () => {
+      this._todoElement.remove();
+    });
+  }
+
+  _generateCheckboxEl() {
+    this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+    this._todoLabel = this._todoElement.querySelector(".todo__label");
+    this._todoCheckboxEl.checked = this._data.completed;
+    this._todoCheckboxEl.id = `todo-${this._data.id}`;
+    this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
   }
 
   getView() {
@@ -17,11 +30,15 @@ class Todo {
       .querySelector(".todo")
       .cloneNode(true);
 
-    const todoNameEl = this._todoElement.querySelector(".todo__name");
-    const todoDate = this._todoElement.querySelector(".todo__date");
-    const todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+    this._todoNameEl = this._todoElement.querySelector(".todo__name");
+    this._todoDate = this._todoElement.querySelector(".todo__date");
+    this._todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
 
-    todoNameEl.textContent = this._data.name;
+    this._todoNameEl.textContent = this._data.name;
+
+    this._generateCheckboxEl();
+
+    this._setEventListeners();
 
     return this._todoElement;
   }
