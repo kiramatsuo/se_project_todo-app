82f944f695cb551816ac7fd24223ab00de10ff44
Kira Matsuo <kira.nm.matsuo@gmail.com>
2025-02-16 14:59:56 -0800

    add TodoCounter class

--- components/todo.js
+++ components/todo.js
@@ -1,19 +1,22 @@
 class Todo {
-  constructor(data, selector) {
+  constructor(data, selector, handleCheck, handleDelete) {
     this._data = data;
     this._templateElement = document.querySelector(selector);
+    this._handleCheck = handleCheck;
+    this._handleDelete = handleDelete;
   }
 
   _setEventListeners() {
-    // check boxes //
     this._todoCheckboxEl.addEventListener("click", () => {
       this._data.completed = !this._data.completed;
       console.log(this._data.completed);
+      this._toggleCompletion();
+      this._handleCheck(this._completed);
     });
 
-    // delete button //
     this._todoDeleteBtn.addEventListener("click", () => {
-      this._todoElement.remove();
+      this._handleDelete(this._completed);
+      this._remove();
     });
   }
 
@@ -38,6 +41,14 @@ class Todo {
       )}`;
     }
   }
+
+  _toggleCompletion = () => {
+    this._completed = !this._completed;
+  };
+
+  _remove = () => {
+    this._todoElement.remove();
+  };
 
   getView() {
     this._todoElement = this._templateElement.content
--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,15 @@ import FormValidator from "../components/FormValidator.js";
 
 import { initialTodos, validationConfig } from "../utils/constants.js";
 
-import Todo from "../components/todo.js";
+import Todo from "../components/Todo.js";
 
 import PopupWithForm from "../components/PopupWithForm.js";
 
 import Section from "../components/Section.js";
 
+import TodoCounter from "../components/TodoCounter.js";
+
+const todoCounter = new TodoCounter(initialTodos, ".counter__text");
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopupEl = document.querySelector("#add-todo-popup");
 const addTodoForm = addTodoPopupEl.querySelector(".popup__form");
@@ -18,10 +21,20 @@ const todoTemplate = document.querySelector("#todo-template");
 const todosList = document.querySelector(".todos__list");
 
 const generateTodo = (data) => {
-  const todo = new Todo(data, "#todo-template");
+  const todo = new Todo(data, "#todo-template", handleCheck, handleDelete);
   const todoElement = todo.getView();
   return todoElement;
 };
+
+function handleCheck(completed) {
+  todoCounter.updateCompleted(completed);
+}
+
+function handleDelete(completed) {
+  if (completed) {
+    todoCounter.updateCompleted(false);
+  }
+}
 
 addTodoButton.addEventListener("click", () => {
   addTodoPopup.open();
